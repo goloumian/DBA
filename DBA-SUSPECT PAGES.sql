05ef8187a732618151c9bcfddca30e2ed1f5e473
Oshin Goloumian <goloumian.o@gmail.com>
2021-08-02 12:24:13 +0430

    Add files via upload

---
+++ DBA-SUSPECT PAGES.sql
@@ -0,0 +1,7 @@
+-- =============================================
+-- Author:      Oshin Goloumian
+-- Create date: 2020-01-07
+-- Description: This query will help you to recognize suspect pages
+-- =============================================
+
+select * from msdb.dbo.suspect_pages
